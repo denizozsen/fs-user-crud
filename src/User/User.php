c699e4cc6a6a56c8d2dc5dceae3cb6d350851888
Deniz Ozsen <deniz.ozsen@gmail.com>
2017-06-29 01:23:10 +0100

    User model: implment and unit test validation

--- src/User/User.php
+++ src/User/User.php
@@ -25,4 +25,42 @@ class User extends Model
     {
         return new DbCrud(get_class(), 'user', 'user_id');
     }
+
+    /**
+     * @throws \ErrorException, if the data in this User is invalid.
+     */
+    public function validate()
+    {
+        $errors = [];
+        if ( isset($this->user_id) && ((string)(int)$this->user_id != $this->user_id) ) {
+            $errors[] = "user_id must be an integer, but was: {$this->user_id}";
+        }
+        if ( !isset($this->email) || !filter_var($this->email, FILTER_VALIDATE_EMAIL) ) {
+            $errors[] = "email must be a valid email address, but was: " . (isset($this->email) ? $this->email : '(not set)');
+        }
+        if ( !isset($this->first_name) || !is_string($this->first_name) || !strlen($this->first_name) ) {
+            $errors[] = "first_name must be a non-empty string, but was: " . (isset($this->first_name) ? $this->first_name : '(not set)');
+        }
+        if ( !isset($this->last_name) || !is_string($this->last_name) || !strlen($this->last_name) ) {
+            $errors[] = "last_name must be a non-empty string, but was: " . (isset($this->last_name) ? $this->last_name : '(not set)');
+        }
+        if ( !isset($this->password) || !is_string($this->password) || !strlen($this->password) ) {
+            $errors[] = "password must be a non-empty string, but was: " . (isset($this->password) ? $this->password : '(not set)');
+        }
+
+        if (count($errors)) {
+            throw new \ErrorException(implode(PHP_EOL, $errors));
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * Throws an \ErrorException, if the data in this User is invalid.
+     */
+    protected function beforeSave()
+    {
+        parent::beforeSave();
+        $this->validate();
+    }
 }
--- tests/User/UserTest.php
+++ tests/User/UserTest.php
@@ -2,7 +2,9 @@
 
 namespace FsTest\User;
 
+use AOrm\Criteria;
 use AOrm\Crud;
+use AOrm\Model;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -12,9 +14,172 @@ use PHPUnit\Framework\TestCase;
  */
 class UserTest extends TestCase
 {
+    /**
+     * {@inheritdoc}
+     *
+     * Overrides all AOrm crud instances with instances of class UserTestMockCrud, defined at the bottom of this file.
+     */
+    public static function setUpBeforeClass()
+    {
+        Model::setCrudOverrideClass(UserTestMockCrud::class);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * Cancels the crud override that was set in setUpBeforeClass().
+     */
+    public static function tearDownAfterClass()
+    {
+        Model::setCrudOverrideClass(null);
+    }
+
+    private function newPopulatedUser()
+    {
+        $user = new User();
+        $user->email = 'a.user@mail.com';
+        $user->first_name = 'A';
+        $user->last_name = 'User';
+        $user->password = 'myPassWord123';
+        return $user;
+    }
+
     public function testCreateCrudInstance_IsACrud()
     {
         $crud_instance = User::createCrudInstance();
         $this->assertInstanceOf(Crud::class, $crud_instance);
+    }
+
+    public function testValidate_Positive1()
+    {
+        $user = $this->newPopulatedUser();
+        $user->validate();
+        $this->assertTrue(true);
+    }
+
+    public function testValidate_Positive2()
+    {
+        $user = $this->newPopulatedUser();
+        $user->user_id = 223344;
+        $user->validate();
+        $this->assertTrue(true);
+    }
+
+    public function testValidate_Negative1()
+    {
+        $this->expectException(\Exception::class);
+        $user = $this->newPopulatedUser();
+        $user->user_id = 'non-numeric';
+        $user->validate();
+    }
+
+    public function testValidate_Negative2()
+    {
+        $this->expectException(\Exception::class);
+        $user = $this->newPopulatedUser();
+        unset($user->email);
+        $user->validate();
+    }
+
+    public function testValidate_Negative3()
+    {
+        $this->expectException(\Exception::class);
+        $user = $this->newPopulatedUser();
+        $user->email = 'invalid.email.com';
+        $user->validate();
+    }
+
+    public function testValidate_Negative4()
+    {
+        $this->expectException(\Exception::class);
+        $user = $this->newPopulatedUser();
+        $user->first_name = '';
+        $user->validate();
+    }
+
+    public function testValidate_Negative5()
+    {
+        $this->expectException(\Exception::class);
+        $user = $this->newPopulatedUser();
+        unset($user->first_name);
+        $user->validate();
+    }
+
+    public function testValidate_Negative6()
+    {
+        $this->expectException(\Exception::class);
+        $user = $this->newPopulatedUser();
+        unset($user->last_name);
+        $user->validate();
+    }
+
+    public function testValidate_Negative7()
+    {
+        $this->expectException(\Exception::class);
+        $user = $this->newPopulatedUser();
+        $user->last_name = '';
+        $user->validate();
+    }
+
+    public function testValidate_Negative8()
+    {
+        $this->expectException(\Exception::class);
+        $user = $this->newPopulatedUser();
+        unset($user->password);
+        $user->validate();
+    }
+
+    public function testValidate_Negative9()
+    {
+        $this->expectException(\Exception::class);
+        $user = $this->newPopulatedUser();
+        $user->password = '';
+        $user->validate();
+    }
+}
+
+/**
+ * Mock implementation for faking the Crud to the AOrm user model and testing what the UserController does.
+ *
+ * @package FsTest\User
+ */
+class UserTestMockCrud implements Crud
+{
+    public function __construct($model_class)
+    {
+        $this->model_class = $model_class;
+    }
+
+    public function getModelClass()
+    {
+        return $this->model_class;
+    }
+
+    public function getPrimaryKey()
+    {
+        return 'user_id';
+    }
+
+    public function fetchOne(Criteria $criteria = null)
+    {
+        return null;
+    }
+
+    public function fetchAll(Criteria $criteria = null)
+    {
+        return [];
+    }
+
+    public function getRelatedJoinFragment($relation)
+    {
+        return null;
+    }
+
+    public function save(array $record)
+    {
+    }
+
+    public function delete($pk_value)
+    {
     }
 }
