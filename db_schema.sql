47a15e9592d73fd5ec544960e65b4e86f58d2bd5
Deniz Ozsen <deniz.ozsen@gmail.com>
2017-06-29 13:19:29 +0100

    Add unique key constraint on the email column in the user table

--- db_schema.sql
+++ db_schema.sql
@@ -8,5 +8,6 @@ CREATE TABLE user (
   last_name VARCHAR(50) NOT NULL,
   password VARCHAR(128) NOT NULL,
   PRIMARY KEY (user_id),
+  UNIQUE KEY (email),
   KEY (user_id, password)
 ) ENGINE InnoDB DEFAULT CHARSET utf8 COLLATE utf8_unicode_ci;
